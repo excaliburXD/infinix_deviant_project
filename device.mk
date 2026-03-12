48646e59aeacddfc09170a992288ed8c08e9af1f
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2026-03-13 01:46:54 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2025 The LineageOS Project
+# Copyright (C) 2026 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
@@ -14,36 +14,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# VAB
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# AB
-ENABLE_VIRTUAL_AB := true
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    system \
-    vendor \
-    product \
-    system_ext \
-    boot \
-    vendor_boot \
-    lk \
-    logo \
-    dtbo \
-    vbmeta \
-    vbmeta_vendor \
-    vbmeta_system
-   
+# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
@@ -54,11 +32,11 @@ PRODUCT_PACKAGES += \
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# API & VNDK
+# API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
 
 # Health Hal
 PRODUCT_PACKAGES += \
@@ -67,21 +45,29 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6785 \
+    bootctrl.mt6785.recovery
+
+PRODUCT_PACKAGES += \
+    create_pl_dev \
+    create_pl_dev.recovery
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# MTK PlPath Utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # Update engine
 PRODUCT_PACKAGES += \
@@ -94,19 +80,19 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Additional configs
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster4 \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster4support \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster_portable \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster_messages \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4 \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4support \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_portable \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_messages \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhwbinder \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhidltransport
 
 TARGET_RECOVERY_DEVICE_MODULES += \
-    keymaster4.so \
-    keymaster4support.so \
-    keymaster_portable.so \
-    keymaster_messages.so \
+    libkeymaster4.so \
+    libkeymaster4support.so \
+    libkeymaster_portable.so \
+    libkeymaster_messages.so \
     libpuresoftkeymasterdevice.so \
     libhwbinder.so \
     libhidltransport.so
