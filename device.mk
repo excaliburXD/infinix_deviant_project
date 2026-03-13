b4fcf2f7773249a0d14c9ccd233ed88dedde1f72
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2026-03-14 02:10:09 +0700

    Deviant: BringUP Tree

--- device.mk
+++ device.mk
@@ -5,41 +5,30 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# VAB
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# AB
-ENABLE_VIRTUAL_AB := true
+# A/B
 AB_OTA_UPDATER := true
-
 AB_OTA_PARTITIONS += \
-    system \
-    vendor \
-    product \
-    system_ext \
     boot \
-    vendor_boot \
-    lk \
-    logo \
     dtbo \
+    lk \
+    preloader \
+    product \
+    system \
+    system_ext \
     vbmeta \
+    vbmeta_system \
     vbmeta_vendor \
-    vbmeta_system
-   
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+    vendor
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -47,41 +36,62 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh
 
-# Dynamic Partition
+# Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# API & VNDK
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
-
-# Health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+# API
+PRODUCT_SHIPPING_API_LEVEL := 28
+BOARD_VNDK_VERSION := current
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery \
-    android.hardware.boot@1.1-mtkimpl
+    android.hardware.boot@1.1-mtkimpl \
+    android.hardware.boot@1.1-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
+    bootctl
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# MTK PlPath Utils
+# create_pl_dev
 PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
+    create_pl_dev \
+    create_pl_dev.recovery
+
+# Gatekeeper 
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service
+
+# Keymaster Beanpod
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-service.beanpod
+
+# Attestation
+PRODUCT_PACKAGES += \
+    vendor.mediatek.hardware.keymaster_attestation@1.1-impl \
+    vendor.mediatek.hardware.keymaster_attestation@1.1-service
+
+# TEE Daemon
+PRODUCT_PACKAGES += \
+    teei_daemon
 
 # Update engine
 PRODUCT_PACKAGES += \
@@ -92,21 +102,23 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# Additional configs
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster4 \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster4support \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster_portable \
-    $(TARGET_OUT_SHARED_LIBRARIES)/keymaster_messages \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libhwbinder \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libhidltransport
-
+# Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
-    keymaster4.so \
-    keymaster4support.so \
-    keymaster_portable.so \
-    keymaster_messages.so \
-    libpuresoftkeymasterdevice.so \
-    libhwbinder.so \
-    libhidltransport.so
+    libkeymaster4 \
+    libkeymaster4support \
+    libkeymaster_messages \
+    libkeymaster_portable \
+    libpuresoftkeymasterdevice \
+    libhwbinder \
+    libhidltransport \
+    wait_for_keymaster
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4support.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_messages.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_portable.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libhwbinder.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libhidltransport.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/wait_for_keymaster
